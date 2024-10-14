45314339e20fdee9076ebfdb44372c8e98862f1e
shivrajkadam19 <shivrajkadam200@gmail.com>
2024-10-14 12:33:34 +0530

    home

--- src/screen/HomeScreen.jsx
+++ src/screen/HomeScreen.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { View,Text } from 'react-native'
+import tw from 'twrnc'
 
 const HomeScreen = () => {
   return (
-    <View>
-        <Text>Home</Text>
+    <View style={tw`flex-1 justify-center items-center`}>
+        <Text style={tw`text-9xl`}>Home</Text>
     </View>
   )
 }
